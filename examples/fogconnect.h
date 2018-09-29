d500e2efb269c87cda276656cee02b645f971b66
LiuzhouChan <llz.chann@gmail.com>
2018-09-29 10:34:34 +0800

    update README and the include and the lib

--- examples/fogconnect.h
+++ examples/fogconnect.h
@@ -71,27 +71,57 @@ void  pr_fogconnect_set_callback(void* ctx, pr_callback_t* callback);
     cbarg: 第三方的私有数据。
     注：在回调函数被调用是需要保存相关的信息。具体看pr_send_peer，pr_event_setcb函数需要使用的参数。
 */
-int  pr_connect_peer(void* ctx, char* peerID, int protocol, int connect_server,
+int  pr_connect_peer(void* ctx, const char* peerID, int protocol, int connect_server,
                       pr_callback_t* callback, void* cbarg);
 
 
-
 /*
-    pr_connect: 为连接后，回调函数传入的参数看pr_callback_t。
+    pr_connect: 为连接后的连接对象。
     recv：传输层受到数据后，调用应用层的数据处理函数（被动调用）。
     close:当连接断开，或超时将被调用。
 */
 int  pr_event_setcb(void* pr_connect, pr_recv_peer_t* recv, pr_close_peer_t* close);
 
 
-
 //向雾节点发送数据。
 /*
-    pr_connect: 为连接后，回调函数传入的参数看pr_callback_t。
+    pr_connect: 为连接后的连接对象。
     buf：待传输的缓存。
     size:缓存数据大小。
 */
 int  pr_send_peer(void* pr_connect, void* buf, int size);
+
+
+//判断节点连接的主/被动方式。
+/*
+    pr_connect: 为连接后的连接对象。
+    返回值：     0表示主动发起链接，1表示被动被连接，
+*/
+int pr_connect_is_passive(void* connect_info);
+
+
+//获取双方约定数，以便处理相关过程。
+/*
+    connect_info: 为连接后的连接对象。
+    返回值：
+   convention_n 各个位的定义。可以看出定义了的分部都是由pr_connect_peer函数的相关参数
+   （int protocol, int connect_server）来确定。
+   31    23    15    7     0
+   |-----|-----|-----|-----|
+                  |     |-------------->低4位（0～3）表示双方约定的传输协议。
+                     |-------->8位(4~11)表示应用层自定定义的约定。
+   |----------->(12~31)未定义。
+*/
+unsigned int  pr_get_convention_number(void* connect_info);
+
+
+
+//被动连接时，设置用户私有数据。
+/*
+    connect_info: 为连接后的连接对象。
+    usrdata：     用户私有数据指针。
+*/
+void pr_connect_set_userdata(void* connect_info, void* usrdata);
 
 
 
@@ -99,5 +129,5 @@ int  pr_send_peer(void* pr_connect, void* buf, int size);
 /*
     char* peer_id: 目前使用mac地址作为peer_id进行识别。
 */
-void set_id(char* peer_id);
+void set_id(const char* ret_id);
 #endif
